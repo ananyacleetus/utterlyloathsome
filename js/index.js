fa630e4ba838609fea502b267895ca23fedc82d3
Ananya W Cleetus <ananyacleetus@gmail.com>
2014-09-29 02:37:10 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 $(function(){
 	var canvas = document.querySelector('canvas'),
       ctx = canvas.getContext('2d'),
-      color = 'white';
+      color = 'black';
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
 	canvas.style.display = 'block';
